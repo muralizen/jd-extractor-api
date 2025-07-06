e94fb91ffd21edf764b8194377231b4e29d0c5bd
muralizen <134152071+muralizen@users.noreply.github.com>
2025-07-06 15:05:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -27,3 +27,8 @@ def extract_jd():
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets this dynamically
+    app.run(host="0.0.0.0", port=port)
